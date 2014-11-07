029236d56bf0f5b1d9c6a1c06c588d435ccc73e6
Cristian Cotes <cristian.cotes@urv.cat>
2014-11-07 11:37:13 +0100

    Initial commit.

---
+++ src/main/java/com/stacksync/stacksync/quotaserver/App.java
@@ -0,0 +1,13 @@
+package com.stacksync.stacksync.quotaserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
